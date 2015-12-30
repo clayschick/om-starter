ea1aa02116f5b2d516494c69295b3a85d46fcbad
clayschick <clay.schick@gmail.com>
2015-12-30 11:47:25 -0600

    finished second part of the basic tutorial

--- project.clj
+++ project.clj
@@ -41,7 +41,7 @@
   :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
              ;; :server-port 3449 ;; default
              ;; :server-ip "127.0.0.1"
-
+             :load-warninged-code true
              :css-dirs ["resources/public/css"] ;; watch and update CSS
 
              ;; Start an nREPL server into the running figwheel process
